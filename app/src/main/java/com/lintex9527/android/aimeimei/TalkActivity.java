2f7b019d4260e1df4f9f1a5f3b46758bf9567d53
LinTeX9527 <LinTeX9527@yeah.net>
2017-09-25 22:07:45 +0800

    Optimize and reformat code.

--- app/src/main/java/com/lintex9527/android/aimeimei/TalkActivity.java
+++ app/src/main/java/com/lintex9527/android/aimeimei/TalkActivity.java
@@ -1,5 +1,6 @@
 package com.lintex9527.android.aimeimei;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,7 +20,6 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 
 
 import com.iflytek.cloud.ErrorCode;
@@ -47,22 +47,24 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class TalkActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = TalkActivity.class.getSimpleName();
 
-    // 语音听写相关
+    // ----------------------------  语音听写相关  -----------------------------
     private SpeechRecognizer mIat;
     // 存储语音识别结果
     private HashMap<String, String> mIatResults = new LinkedHashMap<>();
 
     // 最终的识别结果字符串
     String result_last = null;
-    // 语音识别结果成功还是失败
-    int ret  = 0;
 
-    // --------------------------- 语音合成相关  ---------------------
+    // 语音识别结果成功还是失败
+    int ret = 0;
+
+    // ----------------------------- 语音合成相关  --------------------------------
     // 语音合成器
     private SpeechSynthesizer mTts = null;
     // 发音人
@@ -73,16 +75,13 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     // 播放进度
     private int mPercentForPlaying = 0;
 
-    // 引擎类型
-    private String mEngineType = SpeechConstant.TYPE_CLOUD;
-
-
+    // 弹出消息
     private Toast mToast = null;
 
 
     // 控件相关
-    private EditText editText = null; // 用户在这里输入消息
-    private Button btnSendMsg = null;  // 点击发送用户消息
+    private EditText editText = null;   // 用户在这里输入消息
+    private Button btnSendMsg = null;   // 点击发送用户消息
     private ListView listView = null;   // 加载用户聊天记录
     private Button btnVoice = null;     // 启动语音听写
 
@@ -97,7 +96,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
 
     // 表示数据库中记录的序号
     // 因为数据库中可能已经有了之前的聊天记录，所以需要先获得聊天记录总条数
-    int index = 0;
+    int db_index = 0;
 
     //　Volley　中响应 JsonObjectRequest　的正确返回结果的监听器
     MyResponseListener myResponseListener = null;
@@ -116,7 +115,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 控件初始化
      */
-    private void initView(){
+    private void initView() {
         editText = (EditText) findViewById(R.id.editMsg);
         btnSendMsg = (Button) findViewById(R.id.btnSendMsg);
         listView = (ListView) findViewById(R.id.listview);
@@ -126,11 +125,14 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 在这里初始化机器人相关的信息
      */
-    private void initData(){
+    private void initData() {
+        // 弹出消息
+        mToast = Toast.makeText(TalkActivity.this, "", Toast.LENGTH_SHORT);
+
         robot = TuLing.getUniqueInstance();
         robot.setKey(getResources().getString(R.string.key));
         robot.setName("美眉");
-        robot.setUserid("123456789abc");
+        robot.setUserid(UUID.randomUUID().toString());
         //　TODO:
         //　位置信息还需要优化
         robot.setLoc("上海市人民广场");
@@ -144,39 +146,42 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
         myMsgLists = new ArrayList<>();
         myTextAdapter = new MyTextAdapter(this, myMsgLists);
         listView.setAdapter(myTextAdapter);
+
+        // 当点击ListView中某个项目时，机器人发声
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 TextView tvMsg = (TextView) view.findViewById(R.id.tvMsg);
                 String text = tvMsg.getText().toString();
-                showTip("获取的字符串是：" + text);
                 mTts.startSpeaking(text, mTtsListener);
             }
         });
 
+        // Volley 返回正确结果的处理，更新UI并且机器人发生
         myResponseListener = new MyResponseListener();
 
 
         // --------------------  语音听写相关 --------------------------
-        mIat = SpeechRecognizer.createRecognizer(TalkActivity.this, mInitListener);
-        mToast = Toast.makeText(TalkActivity.this, "", Toast.LENGTH_SHORT);
+        mIat = SpeechRecognizer.createRecognizer(TalkActivity.this, mIATInitListener);
+        // 设置参数
+        setParamIAT();
 
         // --------------------  语音合成相关 -------------------------
         // 初始化合成对象
-        mTts = SpeechSynthesizer.createSynthesizer(TalkActivity.this, mTtsInitListener);
-
+        mTts = SpeechSynthesizer.createSynthesizer(TalkActivity.this, mTTSInitListener);
+        // 语音合成参数初始化
         setParamTTS();
     }
 
     /**
-     * 初始化语音合成监听。
+     * 语音合成初始化监听器。
      */
-    private InitListener mTtsInitListener = new InitListener() {
+    private InitListener mTTSInitListener = new InitListener() {
         @Override
         public void onInit(int code) {
             Log.d(TAG, "InitListener init() code = " + code);
             if (code != ErrorCode.SUCCESS) {
-                showTip("初始化失败,错误码："+code);
+                showTip("初始化失败,错误码：" + code);
             } else {
                 // 初始化成功，之后可以调用startSpeaking方法
                 // 注：有的开发者在onCreate方法中创建完合成对象之后马上就调用startSpeaking进行合成，
@@ -245,7 +250,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 语音合成的参数设置
      */
-    public void setParamTTS(){
+    public void setParamTTS() {
         mTts.setParameter(SpeechConstant.PARAMS, null);
 
         // 设置引擎
@@ -278,7 +283,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 设置语音听写参数
      */
-    public void setParam(){
+    public void setParamIAT() {
         // 清空参数
         mIat.setParameter(SpeechConstant.PARAMS, null);
 
@@ -295,7 +300,6 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
         mIat.setParameter(SpeechConstant.ACCENT, "mandarin");
 
 
-
         // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
         mIat.setParameter(SpeechConstant.VAD_BOS, "4000");
 
@@ -307,14 +311,14 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
 
         // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
         // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-        mIat.setParameter(SpeechConstant.AUDIO_FORMAT,"wav");
-        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory()+"/msc/iat.wav");
+        mIat.setParameter(SpeechConstant.AUDIO_FORMAT, "wav");
+        mIat.setParameter(SpeechConstant.ASR_AUDIO_PATH, Environment.getExternalStorageDirectory() + "/msc/iat.wav");
     }
 
     /**
-     * 初始化监听器。
+     * 语音听写初始化监听器。
      */
-    private InitListener mInitListener = new InitListener() {
+    private InitListener mIATInitListener = new InitListener() {
 
         @Override
         public void onInit(int code) {
@@ -326,8 +330,11 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     };
 
 
-
-    private void showTip(String msg){
+    /**
+     * 弹出消息
+     * @param msg 消息内容
+     */
+    private void showTip(String msg) {
         mToast.setText(msg);
         mToast.setDuration(Toast.LENGTH_SHORT);
         mToast.show();
@@ -336,34 +343,34 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 设定控件响应事件
      */
-    private void initEvent(){
+    private void initEvent() {
         btnSendMsg.setOnClickListener(this);
         btnVoice.setOnClickListener(this);
     }
 
     /**
      * 按钮单击响应事件处理
+     *
      * @param view 响应单击事件的控件，主要是Button
      */
     @Override
     public void onClick(View view) {
 
         // 语音识别初始化失败，直接返回
-        if( null == mIat ){
+        if (null == mIat) {
             // 创建单例失败，与 21001 错误为同样原因，参考 http://bbs.xfyun.cn/forum.php?mod=viewthread&tid=9688
-            this.showTip( "创建对象失败，请确认 libmsc.so 放置正确，且有调用 createUtility 进行初始化" );
+            this.showTip("创建对象失败，请确认 libmsc.so 放置正确，且有调用 createUtility 进行初始化");
             return;
         }
 
-        switch (view.getId()){
+        switch (view.getId()) {
 
             // 启动语音识别
             case R.id.btnVoice:
                 // 移动数据分析，收集开始听写事件
                 FlowerCollector.onEvent(this, "iat_recognize");
                 mIatResults.clear();
-                // 设置参数
-                setParam();
+
 
                 // 不显示听写对话框
                 ret = mIat.startListening(mRecognizerListener);
@@ -380,17 +387,26 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
                 clearMyMessage(); // 点击按钮需要清空输入框
 
                 //　不能发送空消息
-                if (msg.equals("") || msg.trim().equals("")){
-                    Toast.makeText(getBaseContext(), "不要发送空消息", Toast.LENGTH_SHORT).show();
+                if (msg.equals("") || msg.trim().equals("")) {
+                    showTip("不要发送空消息");
                     return;
                 }
 
                 // 添加到数据库
                 db = helper.getWritableDatabase();
                 // 注意下面字符串的前后需要添加单引号
-                index = DBManager.getCount(db, Constant.TABLE_NAME);
-                String sql = "insert into " + Constant.TABLE_NAME + " values(" + (++index) + ", '" + "我" + "', '" + msg + "')";
-                db.execSQL(sql);
+                db_index = DBManager.getCount(db, Constant.TABLE_NAME);
+                // TODO:
+                // 比较这里的数据库插入方法
+                //String sql = "insert into " + Constant.TABLE_NAME + " values(" + (++db_index) + ", '" + "我" + "', '" + msg + "')";
+                //db.execSQL(sql);
+                // 插入一个表情符号，例如 ('^') 使用 sql 语句会出错，但是使用 db.insert() 方法却可以成功，这是为什么？
+                // 看来最好使用数据库API，而不是 sql 语句
+                ContentValues values = new ContentValues();
+                values.put(Constant._ID, ++db_index);
+                values.put(Constant.USER_NAME, "我");
+                values.put(Constant.USER_MESG, msg);
+                db.insert(Constant.TABLE_NAME, null, values);
                 db.close();
 
                 askRobot(msg);
@@ -404,7 +420,12 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    private void askRobot(String msg){
+    /**
+     * 用户向机器人发送消息 msg
+     *
+     * @param msg 用户发送的文本
+     */
+    private void askRobot(String msg) {
 
         // 更新UI
         myMsgLists.add(new MsgEntity(MsgEntity.FLAGS.SENDER, msg));
@@ -427,7 +448,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
                 , new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                Toast.makeText(getBaseContext(), "出错了", Toast.LENGTH_SHORT).show();
+                showTip("Volley返回结果出错了");
             }
         });
 
@@ -439,22 +460,26 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
      * JsonObjectRequest 的返回OK 结果的解析器
      * 这里把返回json格式的结果封装成 TuLingResult 对象，并把它的有用信息保存到数据库中
      */
-    private class MyResponseListener implements Response.Listener<JSONObject>{
+    private class MyResponseListener implements Response.Listener<JSONObject> {
 
         @Override
         public void onResponse(JSONObject jsonObject) {
             TuLingResult tuLingResult = TuLingResult.getUniqueInstance();
             tuLingResult.parseJSONObject(jsonObject);
 
+            // 更新UI
             myMsgLists.add(new MsgEntity(MsgEntity.FLAGS.RECEIVER, tuLingResult.getText()));
             myTextAdapter.notifyDataSetChanged();
+
+            // 朗读机器人返回的结果
+            mTts.startSpeaking(tuLingResult.getText().toString(), mTtsListener);
 
             // 添加到数据库
             db = helper.getWritableDatabase();
             // 更新当前记录的索引号
-            index = DBManager.getCount(db, Constant.TABLE_NAME);
+            db_index = DBManager.getCount(db, Constant.TABLE_NAME);
             // 注意下面字符串的前后需要添加单引号
-            String sql = "insert into " + Constant.TABLE_NAME + " values(" + (++index) + ", '" + robot.getName() + "', '" + tuLingResult.toString() + "')";
+            String sql = "insert into " + Constant.TABLE_NAME + " values(" + (++db_index) + ", '" + robot.getName() + "', '" + tuLingResult.toString() + "')";
             db.execSQL(sql);
             db.close();
         }
@@ -463,12 +488,16 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 清空输入框中用户输入的内容
      */
-    private void clearMyMessage(){
+    private void clearMyMessage() {
         editText.setText("");
     }
 
 
-    private void printResult(RecognizerResult results) {
+    /**
+     * 解析语音识别过程中讯飞语音返回的json对象，最终形成一个字符串，保存在 result_last 之中
+     * @param results 讯飞语音识别语音的结果，多个 results 组成一个完成的识别结果
+     */
+    private void parseRecognizerResult(RecognizerResult results) {
         String text = JsonParser.parseIatResult(results.getResultString());
 
         String sn = null;
@@ -511,8 +540,8 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
             // Tips：
             // 错误码：10118(您没有说话)，可能是录音机权限被禁，需要提示用户打开应用的录音权限。
             // 如果使用本地功能（语记）需要提示用户开启语记的录音权限。
-            if(false && error.getErrorCode() == 14002) {
-                showTip( error.getPlainDescription(true)+"\n请确认是否已开通翻译功能" );
+            if (false && error.getErrorCode() == 14002) {
+                showTip(error.getPlainDescription(true) + "\n请确认是否已开通翻译功能");
             } else {
                 showTip(error.getPlainDescription(true));
             }
@@ -528,11 +557,10 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
         public void onResult(RecognizerResult results, boolean isLast) {
             Log.d(TAG, results.getResultString());
 
-            printResult(results);
+            parseRecognizerResult(results);
 
             if (isLast) {
-                // TODO 最后的结果
-                //showTip("识别结果" + result_last);
+                // 语音识别的结果保存在字符串 result_last 中，需要发送给图灵机器人
                 askRobot(result_last);
 
             }
@@ -541,7 +569,7 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public void onVolumeChanged(int volume, byte[] data) {
             showTip("当前正在说话，音量大小：" + volume);
-            Log.d(TAG, "返回音频数据："+data.length);
+            Log.d(TAG, "返回音频数据：" + data.length);
         }
 
         @Override
@@ -554,6 +582,4 @@ public class TalkActivity extends AppCompatActivity implements View.OnClickListe
             //	}
         }
     };
-
-
 }
